3716ad5c452071460c3b54e7680eefc3c83f5f6b
Tendai Nyandoro <tnyandoro@gmail.com>
2020-11-19 16:30:41 +0200

    delete article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,5 +21,6 @@ class ArticlesController < ApplicationController
     def destroy
        article = Article.find (params[:id])
        article.destroy 
+       redirect_to articles_path
     end
 end
