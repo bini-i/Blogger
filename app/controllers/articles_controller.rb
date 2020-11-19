d56399f7c5f1a0fc30cc10e02a17007d330781e3
bini-i <binyam.sh1@gmail.com>
2020-11-19 18:07:58 +0300

    Update update method and Add Flash notice
    - Flash added to create, destroy, update methods(actions)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,11 +16,26 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         @article.save
 
+        flash.notice = "Article '#{@article.title}' created"
         redirect_to article_path(@article)
     end
+
     def destroy
-       article = Article.find (params[:id])
-       article.destroy 
+       @article = Article.find (params[:id])
+       flash.notice = "Article '#{@article.title}' deleted"
+       @article.destroy 
        redirect_to articles_path
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' updated"
+        redirect_to article_path(@article)
     end
 end
