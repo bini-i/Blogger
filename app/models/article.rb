34e0a9b51d64cbd0ad3ae6ad5ca5bc95042a3637
bini-i <binyam.sh1@gmail.com>
2020-11-20 13:07:50 +0300

    Update article.rb model and _form.html.erb partial
    - Add logic to assign tags to articles using tag_list= method in article
    model
    - Add input field to provide tags for article in _form partial

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,19 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    self.tags.collect do |tag|
+      tag.name
+    end.join(", ")
+  end
+
+  def tag_list=(tags_string) #tags_string = "ruby, technology"
+     tag_names_array = tags_string.split(",").collect{|ele| ele.strip.downcase}.uniq
+
+     new_or_found = tag_names_array.collect {|name| Tag.find_or_create_by(name: name)}
+
+     self.tags = new_or_found     
+  end
 end
